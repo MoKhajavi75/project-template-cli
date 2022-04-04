c834a9cd503354fe4d6b4de5f867c6e1d08e034a
MohamadKh75 <khajavi.contact@gmail.com>
2022-04-04 22:51:05 +0430

    feat: add repo setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-const main = () => {
-  console.log('Hello World!');
+import type { Answers } from './questions';
+import { questions } from './questions';
+import { setup } from './setup';
+
+const main = async () => {
+  const answers: Answers = await questions();
+  await setup(answers);
+
+  console.log('✅ Done!');
 };
 
 main();
---
+++ src/setup.ts
@@ -0,0 +1,26 @@
+import { createDir } from './createDir';
+import type { Answers } from './questions';
+import { shellExecuter } from './shellExecuter';
+
+export const setup = async (answers: Answers) => {
+  const projectDir = `${answers.directory}/${answers.name}`;
+  const templateUrl = 'git@github.com:MohamadKh75/project-template.git';
+
+  await createDir(projectDir);
+
+  await shellExecuter(
+    `cd ${projectDir} \
+  && git clone ${templateUrl} . \
+  && git remote set-url origin ${answers.url} \
+  && pnpm install \
+  && sed -i \
+    -e 's~"name": ".*"~"name": "${answers.name}"~' \
+    -e 's~"description": ".*"~"description": "${answers.description}"~' \
+    -e 's~"url": ".*"~"url": "${answers.url}"~' \
+    package.json \
+  && git reset $(git commit-tree HEAD^{tree} -m "chore: initial commit") \
+  && git add . \
+  && git commit --amend -n --no-edit \
+  && git push origin master -u`
+  );
+};
